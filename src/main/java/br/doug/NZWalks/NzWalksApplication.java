954dae6169d5ec9ebe8a3ae2a1b5e75f5ea56f5f
Doug <douglas@macOs.local>
2025-04-03 22:38:51 -0300

    First commit, folder packages created

---
+++ src/main/java/br/doug/NZWalks/NzWalksApplication.java
@@ -0,0 +1,13 @@
+package br.doug.NZWalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NzWalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NzWalksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/doug/NZWalks/NzWalksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.doug.NZWalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzWalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
